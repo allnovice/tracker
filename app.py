2118c271eccdd6659abc520ec09b61b71368b0f7
allnovice <zepolopmacosusejcire@gmail.com>
2025-09-27 23:31:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>Hello, Render!</h1><p>This is a minimal Flask app.</p>"
-
-if __name__ == "__main__":
-    app.run(debug=True)
